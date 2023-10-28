95acdce8521d678a2a44d410a1714ffbb692579b
chengivan2 <chengoleivan@gmail.com>
2023-10-28 19:34:32 +0300

    Added the press event

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
@@ -7,6 +8,7 @@ import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -14,7 +16,7 @@ export default function App() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
     }
@@ -22,10 +24,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-      <Button theme="primary" label="Choose a photo" />
+      <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
